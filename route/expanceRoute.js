ede168eb19dfd0889875417862e775fe57cc96df
Pradyumna joshi <148041767+Pradyumnajoshi2004@users.noreply.github.com>
2025-06-05 13:18:28 +0530

    Update expanceRoute.js

--- route/expanceRoute.js
+++ route/expanceRoute.js
@@ -1,13 +1,14 @@
 const {getExpance,postExpance,deleteExpance, expanceFindById, updateExpance} = require("../controller/expanceController")
+const auth = require("../middleware/auth")
 const route = require("express").Router()
 
 route.get("/",getExpance)
 
-route.post("/",postExpance)
+route.post("/",auth,postExpance)
 
-route.delete("/:id",deleteExpance)
+route.delete("/:id",auth,deleteExpance)
 
-route.put("/:id",updateExpance)
+route.put("/:id",auth,updateExpance)
 
 route.get("/:id",expanceFindById)
 
